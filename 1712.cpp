6a1309cceba8ec5d2d5174682c12ae04141d6a81
Son Jae Woo <jaewoo1940@unist.ac.kr>
2021-04-23 01:11:51 +0900

    210423

---
+++ 1712.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	if (b >= c)
+	{
+		std::cout << "-1";
+	}
+	else
+	{
+		std::cout << (a / (c - b)) + 1;
+	}
+	return 0;
+}
